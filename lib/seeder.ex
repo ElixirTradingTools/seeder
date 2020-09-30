2df2c6001fb8b5b1c80dc550d446227fe23f98ca
Rob <rm.rf.etc@gmail.com>
2020-09-30 01:56:12 -0700

    Add support for Ameritrade downloader

--- lib/seeder.ex
+++ lib/seeder.ex
@@ -16,13 +16,29 @@ defmodule Seeder do
     field :ticker, String.t()
     field :api_key, String.t()
     field :period_size, pos_integer(), default: 1
-    field :period_unit, :second | :minute | :hour | :day | :week | :month
+    field :period_unit, :second | :minute | :hour | :day | :week | :month | nil
     field :start, Date.t()
     field :end, Date.t()
     field :db_root, String.t() | nil, default: nil
   end
 
-  @base_path 'https://api.polygon.io/v2/aggs/ticker'
+  @polygon_url 'https://api.polygon.io/v2/aggs/ticker'
+  @ameritrade_url 'https://api.tdameritrade.com/v1/marketdata'
+
+  def get_ameritrade_path(%Conf{
+        ticker: sym,
+        api_key: key,
+        period_size: period_n,
+        period_unit: period_u
+      }) do
+    ["apikey=#{key}", "frequencyType=#{period_u}", "frequency=#{period_n}"]
+    |> Enum.join("&")
+    |> case do
+      query_string ->
+        "#{@ameritrade_url}/#{sym}/pricehistory?#{query_string}"
+    end
+    |> to_charlist()
+  end
 
   def convert_map_keys_to_atoms(map) do
     map
@@ -30,18 +46,35 @@ defmodule Seeder do
     |> Map.new()
   end
 
-  def download(%Conf{
+  def ameritrade_dl(conf = %Conf{}) do
+    conf
+    |> get_ameritrade_path()
+    |> :httpc.request()
+    |> case do
+      {:ok, {_response, _headers, body}} -> body
+    end
+    |> to_string()
+    |> Jason.decode!()
+    |> Map.get("candles")
+    |> Enum.map(fn
+      %{"open" => o, "high" => h, "low" => l, "close" => c, "volume" => v, "datetime" => t} ->
+        %{t: t, o: "#{o}", h: "#{h}", l: "#{l}", c: "#{c}", v: v}
+    end)
+  end
+
+  def polygon_dl(%Conf{
+        ticker: sym,
+        api_key: api_key,
         start: start_date,
         end: end_date,
-        ticker: sym,
         period_size: time_n,
-        period_unit: time_u,
-        api_key: api_key
+        period_unit: time_u
       }) do
     start_date = Date.to_iso8601(start_date)
     end_date = Date.to_iso8601(end_date)
+    path = Enum.join([sym, "range", time_n, time_u, start_date, end_date], "/")
 
-    '#{@base_path}/#{sym}/range/#{time_n}/#{time_u}/#{start_date}/#{end_date}?apiKey=#{api_key}'
+    '#{@polygon_url}/#{path}?apiKey=#{api_key}'
     |> :httpc.request()
     |> case do
       {:ok, {_response, _headers, body}} -> body
@@ -105,11 +138,16 @@ defmodule Seeder do
     {:ok, db}
   end
 
-  def dl_to_db(conf) do
+  def dl_to_db(conf, api_service) when api_service in [:polygon, :ameritrade] do
     if(!File.dir?(conf.db_root), do: {:error, :invalid_db_root})
 
     {:ok, db} = chart_db_handler(conf)
-    bars = download(conf)
+
+    bars =
+      case api_service do
+        :polygon -> polygon_dl(conf)
+        :ameritrade -> ameritrade_dl(conf)
+      end
 
     Enum.each(bars, fn %{t: t, o: o, h: h, l: l, c: c, v: v} ->
       Depo.write(db, :add, [t, "#{o}", "#{h}", "#{l}", "#{c}", v])
